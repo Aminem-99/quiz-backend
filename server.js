32e01a708702742815c7bd1178464d6264a38ee1
Aminem-99 <amine.manzeh@dauphine.eu>
2025-07-20 21:21:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import cors from 'cors';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
+
+dotenv.config();
+
 console.log("SUPABASE_ANON_KEY =", process.env.SUPABASE_ANON_KEY);
 console.log("SUPABASE_URL =", process.env.SUPABASE_URL);
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
